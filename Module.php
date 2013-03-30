d604b48387e1b74a6d1cba8d36684803d7942ff4
Fredrik Wahlqvist <fwahlqvist@gmail.com>
2013-03-30 23:04:02 +0000

    Adding missing Module.php

---
+++ Module.php
@@ -0,0 +1,48 @@
+<?php
+namespace WdevPresentation;
+
+use WdevPresentation\Model\Presentation;
+use WdevPresentation\Model\PresentationTable;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+
+class Module
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'WdevPresentation\Model\PresentationTable' =>  function($sm) {
+                    $tableGateway = $sm->get('PresentationTableGateway');
+                    $table = new PresentationTable($tableGateway);
+                    return $table;
+                },
+                'PresentationTableGateway' => function ($sm) {
+                    $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Presentation());
+                    return new TableGateway('presentation', $dbAdapter, null, $resultSetPrototype);
+                },
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+}
