5825ff11c3350f052fe9ef1ed845158d9660ad0b
Fredrik Wahlqvist <fwahlqvist@gmail.com>
2013-04-08 22:31:55 +0100

    Working copy

--- src/WdevPresentation/Form/PresentationForm.php
+++ src/WdevPresentation/Form/PresentationForm.php
@@ -12,14 +12,14 @@ class PresentationForm extends Form
         $this->setAttribute('method', 'post');
         $this->add(array(
             'name' => 'id',
+            'type'  => 'hidden',
             'attributes' => array(
-                'type'  => 'hidden',
             ),
         ));
         $this->add(array(
             'name' => 'title',
+            'type'  => 'text',
             'attributes' => array(
-                'type'  => 'text',
             ),
             'options' => array(
                 'label' => 'Title',
@@ -27,14 +27,289 @@ class PresentationForm extends Form
         ));
         $this->add(array(
             'name' => 'content',
+            'type'  => 'textarea',
             'attributes' => array(
-                'type'  => 'textarea',
-                'rows' => '10'
+                'cols' => '100',
+                'rows' => '20',
+                'value' => '<div class="reveal">
+                                <div class="slides">
+                                    <section>Single Horizontal Slide</section>
+                                    <section>
+                                        <section>Vertical Slide 1</section>
+                                        <section>Vertical Slide 2</section>
+                                    </section>
+                                </div>
+                            </div>'
             ),
             'options' => array(
                 'label' => 'Content',
             ),
         ));
+        
+        $this->add(array(
+            'name' => 'private',
+            'type' => 'Checkbox',
+            'attributes' => array(
+            ),
+            'options' => array(
+                'label' => 'Private',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'theme',
+            'type' => 'text',
+            'attributes' => array(
+                'list' => 'themes',
+                'placeholder' => 'default, beige, sky, night, serif, simple',
+                'value' => 'default',
+            ),
+            'options' => array(
+                'label' => 'Theme',
+            ),
+        ));
+        
+        
+        $this->add(array(
+            'name' => 'controls',
+            'type' => 'Checkbox',
+            'attributes' => array(
+            ),
+            'options' => array(
+                'label' => 'Controls',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'progress',
+            'type' => 'Checkbox',
+            'attributes' => array(
+            ),
+            'options' => array(
+                'label' => 'Progress',
+            ),
+        ));
+           
+        $this->add(array(
+            'name' => 'history',
+            'type' => 'Checkbox',
+            'attributes' => array(
+            ),
+            'options' => array(
+                'label' => 'History',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'center',
+            'type' => 'Checkbox',
+            'attributes' => array(
+            ),
+            'options' => array(
+                'label' => 'Center',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'autoSlide',
+            'type' => 'text',
+            'attributes' => array(
+            ),
+            'options' => array(
+                'label' => 'Auto Slide',
+            ),
+        ));
+            
+        $this->add(array(
+            'name' => 'keyboard',
+            'type' => 'Checkbox',
+            'attributes' => array(
+            ),
+            'options' => array(
+                'label' => 'Keyboard',
+            ),
+        ));
+            
+        $this->add(array(
+            'name' => 'overview',
+            'type' => 'Checkbox',
+            'attributes' => array(
+            ),
+            'options' => array(
+                'label' => 'Overview',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'looop',
+            'type' => 'Checkbox',
+            'attributes' => array(
+            ),
+            'options' => array(
+                'label' => 'Loop',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'rtl',
+            'type' => 'Checkbox',
+            'attributes' => array(
+            ),
+            'options' => array(
+                'label' => 'RTL',
+            ),
+        ));
+            
+        $this->add(array(
+            'name' => 'mouseWeel',
+            'type' => 'Checkbox',
+            'attributes' => array(
+            ),
+            'options' => array(
+                'label' => 'Mouse Weel',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'rollingLinks',
+            'type' => 'Checkbox',
+            'attributes' => array(
+            ),
+            'options' => array(
+                'label' => 'Rolling Links',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'transition',
+            'type' => 'Select',
+            'attributes' => array(
+                'options' => array(
+                    'default' =>'Default',
+                    'cube' => 'Cube',
+                    'page' => 'Page',
+                    'concave' => 'Concave',
+                    'zoom' => 'Zoom',
+                    'linear' => 'Linear',
+                    'fade' => 'Fade',
+                    'none' => 'None',
+                ),
+            ),
+            'options' => array(
+                'label' => 'Transition',
+            ),
+        ));
+            
+        $this->add(array(
+            'name' => 'presetationWidth',
+            'type' => 'text',
+            'attributes' => array(
+                'placeholder' => '960',
+            ),
+            'options' => array(
+                'label' => 'Presentation Width',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'presentationHeight',
+            'type' => 'text',
+            'attributes' => array(
+                'placeholder' => '700',
+            ),
+            'options' => array(
+                'label' => 'Presentation Height',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'presentationUnit',
+            'type' => 'Select',
+            'attributes' => array(
+                'options' => array(
+                    'pixels' =>'Pixels',
+                    'percentage' => 'Percentage',
+                ),
+            ),
+            'options' => array(
+                'label' => 'Presentation Unit',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'presentationMargin',
+            'type' => 'text',
+            'attributes' => array(
+                'placeholder' => '0.1',
+            ),
+            'options' => array(
+                'label' => 'Presenation Margin',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'presentationMinScale',
+            'type' => 'text',
+            'attributes' => array( 
+                'placeholder' => '0.2',
+            ),
+            'options' => array(
+                'label' => 'Presentation Min Scale',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'presentationMaxScale',
+            'type' => 'text',
+            'attributes' => array(
+                'placeholder' => '1.0',
+            ),
+            'options' => array(
+                'label' => 'Presentation Max Scale',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'multiplexId',
+            'type' => 'text',
+            'attributes' => array(
+                'placeholder' => 'e3ea5543b7e6bb65',
+            ),
+            'options' => array(
+                'label' => 'Multiplex ID',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'multiplexSecret',
+            'type' => 'text',
+            'attributes' => array(
+                'placeholder' => '12345678910111213',
+            ),
+            'options' => array(
+                'label' => 'Multiplex Secret',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'multiplexUrl',
+            'type' => 'Url',
+            'attributes' => array(
+                'placeholder' => 'http://wahlqvist.co.uk',
+            ),
+            'options' => array(
+                'label' => 'multiplexUrl',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'timestamp',
+            'type' => 'hidden',
+            'attributes' => array(
+            ),
+        ));
+            
+        
         $this->add(array(
             'name' => 'submit',
             'attributes' => array(
--- src/WdevPresentation/Model/Presentation.php
+++ src/WdevPresentation/Model/Presentation.php
@@ -11,6 +11,31 @@ class Presentation implements InputFilterAwareInterface
     public $id;
     public $title;
     public $content;
+    public $private;
+    public $theme;
+    public $controls;
+    public $history;
+    public $center;
+    public $autoSlide;
+    public $keyboard;
+    public $overview;
+    public $looop;
+    public $rtl;
+    public $mouseWeel;
+    public $rollingLinks;
+    public $transition;
+    public $presetationWidth;
+    public $presentationHeight;
+    public $presentationUnit;
+    public $presentationMargin;
+    public $presentationMinScale;
+    public $presentationMaxScale;
+    public $multiplexId;
+    public $multiplexSecret;
+    public $multiplexUrl;
+    public $timestamp;
+    
+    
     protected $inputFilter;    
     
     public function exchangeArray($data)
@@ -18,6 +43,30 @@ class Presentation implements InputFilterAwareInterface
         $this->id     = (isset($data['id'])) ? $data['id'] : null;
         $this->title = (isset($data['title'])) ? $data['title'] : null;
         $this->content  = (isset($data['content'])) ? $data['content'] : null;
+        $this->private  = (isset($data['private'])) ? $data['private'] : null;
+        $this->theme  = (isset($data['theme'])) ? $data['theme'] : null;
+        $this->controls  = (isset($data['controls'])) ? $data['controls'] : null;
+        $this->progress  = (isset($data['progress'])) ? $data['progress'] : null;
+        $this->history  = (isset($data['history'])) ? $data['history'] : null;
+        $this->center  = (isset($data['center'])) ? $data['center'] : null;
+        $this->autoSlide  = (isset($data['autoSlide'])) ? $data['autoSlide'] : null;
+        $this->keyboard  = (isset($data['keyboard'])) ? $data['keyboard'] : null;
+        $this->overview  = (isset($data['overview'])) ? $data['overview'] : null;
+        $this->looop  = (isset($data['looop'])) ? $data['looop'] : null;
+        $this->rtl  = (isset($data['rtl'])) ? $data['rtl'] : null;
+        $this->mouseWeel  = (isset($data['mouseWeel'])) ? $data['mouseWeel'] : null;
+        $this->rollingLinks  = (isset($data['rollingLinks'])) ? $data['rollingLinks'] : null;
+        $this->transition  = (isset($data['transition'])) ? $data['transition'] : null;
+        $this->presetationWidth  = (isset($data['presetationWidth'])) ? $data['presetationWidth'] : null;
+        $this->presentationHeight  = (isset($data['presentationHeight'])) ? $data['presentationHeight'] : null;
+        $this->presentationUnit  = (isset($data['presentationUnit'])) ? $data['presentationUnit'] : null;
+        $this->presentationMargin  = (isset($data['presentationMargin'])) ? $data['presentationMargin'] : null;
+        $this->presentationMinScale  = (isset($data['presentationMinScale'])) ? $data['presentationMinScale'] : null;
+        $this->presentationMaxScale  = (isset($data['presentationMaxScale'])) ? $data['presentationMaxScale'] : null;
+        $this->multiplexId  = (isset($data['multiplexId'])) ? $data['multiplexId'] : null;
+        $this->multiplexSecret  = (isset($data['multiplexSecret'])) ? $data['multiplexSecret'] : null;
+        $this->multiplexUrl  = (isset($data['multiplexUrl'])) ? $data['multiplexUrl'] : null;
+        $this->timestamp  = (isset($data['timestamp'])) ? $data['timestamp'] : null;
     }
     
     public function getArrayCopy()
@@ -66,14 +115,14 @@ class Presentation implements InputFilterAwareInterface
             $inputFilter->add($factory->createInput(array(
                 'name'     => 'content',
                 'required' => true,
-                /*'filters'  => array(
+                'filters'  => array(
                     array('name' => 'StripTags',
                         'options' => array(
-                            'allowTags' => array('p','strong','em','u','h1','h2','h3','h4','h5','h6','img','li','ol','ul','span','div','br','ins','del', 'a','pre', '<!--', '-->', 'section', 'aside', 'script', 'small', 'code', 'blockquote', 'iframe', 'form'),
+                            'allowTags' => array('p','strong','em','u','h1','h2','h3','h4','h5','h6','img','li','ol','ul','span','div','br','ins','del', 'a','pre', '<!--', '-->', 'section', 'aside', 'script', 'small', 'code', 'blockquote', 'quote', 'iframe', 'form', 'table', 'tr', 'th', 'td', ),
                             'allowAttribs' => array('href', 'title', 'src', 'class','width', 'height', 'type', 'id', 'data-state','contenteditable', 'cite', 'target', 'action', 'src'),
                         )),
                     array('name' => 'StringTrim'),
-                ),*/
+                ),
                 'validators' => array(
                     array(
                         'name'    => 'StringLength',
@@ -85,6 +134,326 @@ class Presentation implements InputFilterAwareInterface
                     ),
                 ),
             )));
+            
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'private',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int',
+                        'options' => array( 
+                        'min' => 0, 
+                        'max' => 1,
+                        ),
+                    ),
+                ),
+            )));
+                
+                
+            $inputFilter->add($factory->createInput(array(
+                'name' => 'theme',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'controls',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int',
+                        'options' => array( 
+                        'min' => 0, 
+                        'max' => 1,
+                        ),
+                    ),
+                ),
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'progress',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int',
+                        'options' => array( 
+                        'min' => 0, 
+                        'max' => 1,
+                        ),
+                    ),
+                ),
+            )));
+            
+       
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'history',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int',
+                        'options' => array( 
+                        'min' => 0, 
+                        'max' => 1,
+                        ),
+                    ),
+                ),
+            )));
+        
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'center',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int',
+                        'options' => array( 
+                        'min' => 0, 
+                        'max' => 1,
+                        ),
+                    ),
+                ),
+            )));
+        
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'autoSlide',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int',
+                        'options' => array( 
+                        'min' => 0, 
+                        'max' => 9999999999,
+                        ),
+                    ),
+                ),
+            )));
+        
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'keyboard',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int',
+                        'options' => array( 
+                        'min' => 0, 
+                        'max' => 1,
+                        ),
+                    ),
+                ),
+            )));
+        
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'overview',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int',
+                        'options' => array( 
+                        'min' => 0, 
+                        'max' => 1,
+                        ),
+                    ),
+                ),
+            )));
+            
+        
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'looop',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int',
+                        'options' => array( 
+                        'min' => 0, 
+                        'max' => 1,
+                        ),
+                    ),
+                ),
+            )));
+        
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'rtl',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int',
+                        'options' => array( 
+                        'min' => 0, 
+                        'max' => 1,
+                        ),
+                    ),
+                ),
+            )));
+            
+        
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'mouseWeel',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int',
+                        'options' => array( 
+                        'min' => 0, 
+                        'max' => 1,
+                        ),
+                    ),
+                ),
+            )));
+        
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'rollingLinks',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int',
+                        'options' => array( 
+                        'min' => 0, 
+                        'max' => 1,
+                        ),
+                    ),
+                ),
+            )));
+            
+        
+            $inputFilter->add($factory->createInput(array(
+                'name' => 'transition',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'presentationWidth',
+                'filters'  => array(
+                    array('name' => 'Int',
+                    ),
+                ),
+            )));
+        
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'presentationHeight',
+                'filters'  => array(
+                    array('name' => 'Int'
+                    ),
+                ),
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name' => 'presentationUnit',
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 10,
+                        ),
+                    ),
+                ),
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'presentationMargin',
+                'filters'  => array(
+                    array('name' => 'Int',
+                        'options' => array( 
+                        'min' => 0, 
+                        'max' => 4,
+                        ),
+                    ),
+                ),
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'presentationMinScale',
+                'filters'  => array(
+                    array('name' => 'Int',
+                        'options' => array( 
+                        'min' => 0, 
+                        'max' => 1,
+                        ),
+                    ),
+                ),
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'presentationMaxScale',
+                'filters'  => array(
+                    array('name' => 'Int',
+                        'options' => array( 
+                        'min' => 0, 
+                        'max' => 1,
+                        ),
+                    ),
+                ),
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'multiplexSecret',
+                'filters'  => array(
+                    array('name' => 'Int',
+                        'options' => array( 
+                        'min' => 0, 
+                        'max' => 1,
+                        ),
+                    ),
+                ),
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'  => 'multiplexId',
+                'filters'  => array(
+                    array('name' => 'Int',
+                        'options' => array( 
+                        'min' => 0, 
+                        'max' => 1,
+                        ),
+                    ),
+                ),
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name' => 'multiplexUrl',
+                'required' => false,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 255,
+                        ),
+                    ),
+                ),
+            )));
+        
+            
+            
 
             $this->inputFilter = $inputFilter;
         }
